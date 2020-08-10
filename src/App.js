0943689f3bb7ce3d15430eb917bb744a3ef3600f
Osamaarif <usamaarif74@yahoo.com>
2020-08-11 00:08:52 +0500

    maybe

--- src/App.js
+++ src/App.js
@@ -4,13 +4,10 @@ import './App.css';
 
 import { makeStyles } from '@material-ui/core/styles';
 
-import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 
 import {
   BrowserRouter as Router,
